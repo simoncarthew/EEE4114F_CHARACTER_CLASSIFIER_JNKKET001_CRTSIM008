00b8acfb4e1a6a61c9e27911b31f8f8840ac0e29
simoncarthew <carthewsimone@gmail.com>
2024-05-13 12:58:16 +0200

    Big Pivot here

--- scripts/building.py
+++ scripts/building.py
@@ -4,51 +4,61 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import datasets
 import torchvision.transforms as transforms
 import torchvision.io as io
 from torch.utils.data import DataLoader, TensorDataset
+from matplotlib.pyplot import imshow
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
 import random
 import os
 import sys
-import string
 
-################## MISC FUNCTIONS ###################
+#### MISC FUNCTIONS #####
 
-# split dataset into training validation and testing
-def getSplitData(data, test_size=0.2, test = True):
+# load MNIST training and test data
+def getProcessedData():
+    # get the data from the mnsit data
+    train = datasets.MNIST(".", train=True, download=False)
+    test = datasets.MNIST(".", train=False, download=False)
 
-    # Extract the image data into X and labels into y
-    y = data['0']
-    X = data.drop('0', axis=1)
-    X = X.astype(float)
+    # extract the image data into x and labels into y
+    x_train = train.data.float()
+    y_train = train.targets
+    x_test = test.data.float()
+    y_test = test.targets
 
-    # Split into training and testing data
-    X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=test_size, random_state=42)
+    # convert the training and test data into numpy array
+    x_train = x_train.numpy()
+    y_train = y_train.numpy()
+    x_test = x_test.numpy()
+    y_test = y_test.numpy()
 
-    # Create validation data
-    if test:
-        X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=test_size, random_state=42)
-    else:
-        X_valid, y_valid = None, None
+    # create validation data
+    test_size = x_test.shape[0]
+    indices = np.random.choice(x_train.shape[0], test_size, replace=False)
 
-    # Reshape the image data into 2D arrays
-    X_train = X_train.reshape(-1, 28 * 28)
-    X_valid = X_valid.reshape(-1, 28 * 28) if X_valid is not None else None
-    X_test = X_test.reshape(-1, 28 * 28)
+    x_valid = x_train[indices]
+    y_valid = y_train[indices]
 
-    return X_train, y_train, X_valid, y_valid, X_test, y_test
+    # remove validation set from training set
+    x_train = np.delete(x_train, indices, axis=0)
+    y_train = np.delete(y_train, indices, axis=0)
 
-# make datasets 
+    # flatten the image data into 1D arrays
+    x_train = x_train.reshape(-1, 28*28)
+    x_valid = x_valid.reshape(-1,28*28)
+    x_test = x_test.reshape(-1, 28*28)
+
+    return x_train, y_train, x_valid, y_valid,  x_test, y_test
+
 def makeDatasets(x_train, y_train, x_valid, y_valid,  x_test, y_test):
     train_dataset = TensorDataset(torch.from_numpy(x_train), torch.from_numpy(y_train))
     val_dataset = TensorDataset(torch.from_numpy(x_valid), torch.from_numpy(y_valid))
     test_dataset = TensorDataset(torch.from_numpy(x_test), torch.from_numpy(y_test))
     return train_dataset, val_dataset, test_dataset
 
-# make data loaders
 def make_DataLoaders(train_dataset, val_dataset, test_dataset, batch_size):
     train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size, shuffle=True)
@@ -64,17 +74,16 @@ def JPGtoTensor(file_path,device):
     image = image.to(device)
     return image
 
-# display image
 def display_image(X_i, title):
     plt.imshow(X_i, cmap='binary')
     plt.title(title)
     plt.show()
 
-################## ANN MODEL ###################
+#### ANN MODEL ####
 
-class LetterClassifier(nn.Module):
-    def __init__(self, input_size=28*28, num_layers=3, layer_sizes=[28*28,28*28], output_size=10, activation=F.relu, dropout_rate = 0):
-        super(LetterClassifier, self).__init__()
+class NumberClassifier(nn.Module):
+    def __init__(self, input_size=28*28, num_layers=3, layer_sizes=[28*28,28*28,28*28], output_size=10, activation=F.relu, dropout_rate = 0):
+        super(NumberClassifier, self).__init__()
         
         self.layers = nn.ModuleList()
         
@@ -99,7 +108,6 @@ class LetterClassifier(nn.Module):
         feature = torch.softmax(self.output_layer(feature), dim=1)
         return feature
 
-# used to train a single letter classifier model
 def train_classifier(model,train_loader, val_loader,max_no_epochs, min_loss_chng,learn_rate, display=False, loss_func=nn.CrossEntropyLoss()):
     # initialize previous loss function and
     optimizer = torch.optim.SGD(model.parameters(),lr=learn_rate)
@@ -143,12 +151,9 @@ def train_classifier(model,train_loader, val_loader,max_no_epochs, min_loss_chng
                 no_min_change = 0
             if no_min_change > 4:
                 return validation_trend, training_trend, loss_trend, epochs
-    
-    del optimizer
-    del loss_func
+            
     return validation_trend, training_trend, loss_trend, epochs
 
-# used to classify a single letter classifier
 def validate_classifier(model, test_loader, max_batches = None):
     model.eval()  # set model to evaluation mode
     correct = 0
@@ -169,11 +174,34 @@ def validate_classifier(model, test_loader, max_batches = None):
 
     return accuracy
 
-# make prediction using LetterClassifier
 def makePrediction(model,image):
     output = model(torch.tensor(image).view(1, -1))
     _, predicted = torch.max(output, 1)
     return predicted
+
+def validate_jpgs(model, device, dir_path):
+    correct = 0
+    total = 0
+    numbers = [0,1,2,3,4,5,6,7,8,9]
+
+    # loop through every letter/class in alphabet
+    for class_num in numbers:
+        # set the class directory path
+        class_dir_pth = dir_path + "/" + str(class_num)
+
+        # get all the jpg files in the class directory
+        files = os.listdir(class_dir_pth)
+        jpg_files = [file for file in files if file.lower().endswith('.jpg')]
+
+        for file in jpg_files:
+            image = JPGtoTensor(class_dir_pth + "/" + file,device=device)
+            predicted = makePrediction(model,image)
+            total += 1
+            if predicted == class_num:
+                correct += 1
+
+    accuracy = 100 * correct / total
+    return accuracy
 
 # save the current state of a model
 def save_model(model,file_path):
@@ -181,236 +209,6 @@ def save_model(model,file_path):
 
 # load a previously trained model
 def load_model(file_path):
-    model = LetterClassifier()
+    model = NumberClassifier()
     model.load_state_dict(torch.load(file_path))
     return model
-
-############### COMBINED LETTER CLASSIFIER ANNS ###############
-class CombLetterClassifier:
-    # initialize
-    def __init__(self, num_models, device,input_size=28*28, num_layers=3, layer_sizes=[28*28,28*28,28*28], output_size=26, activation=F.relu, dropout_rate = 0, create_models=True,model_targets = []):
-        # initlialize standard variables
-        self.num_models = num_models
-        self.models = []
-        self.device = device
-        self.model_targets = model_targets
-        self.output_size = output_size
-
-        # split output classes among models
-        if len(self.model_targets) == 0 or len(self.model_targets) != self.num_models:
-            print("Using default target split")
-            quotient, remainder = divmod(output_size, num_models)
-            self.output_sizes =  [quotient + 1] * remainder + [quotient] * (num_models - remainder)
-        else:
-            self.output_sizes = []
-            for i in range(len(model_targets)):
-                self.output_sizes.append(len(model_targets[i]))
-
-        # create the relevant number of models
-        if create_models:
-            i = 0
-            for o_s in self.output_sizes:
-                self.models.append(LetterClassifier(input_size=input_size, num_layers=num_layers, layer_sizes=layer_sizes, output_size=o_s, activation=activation, dropout_rate = dropout_rate))
-                self.models[i].to(self.device)
-                i=i+1
-
-    # create the data loders for each model
-    def createDataLoaders(self,data_file_path,batch_size):
-        # intialise dataloader arrays
-        self.train_loaders = []
-        self.test_loaders = []
-        self.val_loaders = []
-
-        # load the entire dataset
-        data = pd.read_csv(data_file_path)
-
-        start = 0
-        for i in range(self.num_models):   # for loop where each iteration creates new set of dataloaders per model
-            print("Making loaders for model ",i, "with num_classes = ",self.output_sizes[i])
-            print("start =",start)
-
-            # calculate class intervals
-            end = start + self.output_sizes[i]
-
-            # reduce dataset to specific classes
-            downsampled_data = []
-            if len(self.model_targets) == 0 or len(self.model_targets) != self.num_models:
-                for j in range(start,end):
-                    class_data = data[data.iloc[:, 0] == j]
-                    class_data[data.columns[0]] = class_data[data.columns[0]] - start
-                    downsampled_data.append(class_data)
-                downsampled_data = pd.concat(downsampled_data)
-            else:
-                for j in range(0,self.output_size):
-                    if j in self.model_targets[i]:
-                        class_data = data[data.iloc[:, 0] == j]
-                        class_data[data.columns[0]] = self.model_targets[i].index(j)
-                        downsampled_data.append(class_data)
-                downsampled_data = pd.concat(downsampled_data)
-
-            # split downsampled data into train val and test
-            X_train, y_train, X_valid, y_valid, X_test, y_test = getSplitData(downsampled_data, test_size=0.2, test = True)
-
-            # make datasets and move to device
-            train_dataset,val_dataset,test_dataset = makeDatasets(X_train,y_train,X_valid, y_valid, X_test,y_test)
-            train_dataset = [(x.to(self.device), y.to(self.device)) for x, y in train_dataset]
-            val_dataset = [(x.to(self.device), y.to(self.device)) for x, y in val_dataset]
-            test_dataset = [(x.to(self.device), y.to(self.device)) for x, y in test_dataset]
-            print(np.unique(y_train))
-
-            # make dataloaders and add to arrays
-            train_loader, val_loader, test_loader = make_DataLoaders(train_dataset,val_dataset,test_dataset,batch_size)
-            self.train_loaders.append(train_loader)
-            self.val_loaders.append(val_loader)
-            self.test_loaders.append(test_loader)
-
-            start = end
-
-    # train all the models on thier relevant training data
-    def train(self,max_no_epochs, min_loss_chng,learn_rate, display=False, loss_func=nn.CrossEntropyLoss()):
-        for i in range(self.num_models):
-            print("Training model:",i)
-            train_classifier(self.models[i],self.train_loaders[i],self.val_loaders[i],max_no_epochs,min_loss_chng,learn_rate,display=display,loss_func=loss_func)
-            print("Validating model:",i)
-            test_acc = validate_classifier(self.models[i],self.test_loaders[i])
-            print(f"Test Set Validation Accuracy: {test_acc:.2f}%")
-    
-    # classify single image
-    def classify(self,image):
-
-        # get the predictions from each model
-        max_preds = []
-        for i in range(self.num_models):
-            output = self.models[i](image.clone().detach().view(1, -1))
-            max_value, predicted = torch.max(output, 1) # find the maximum prediction
-            if len(self.model_targets) == 0:
-                max_preds.append([max_value.item(),predicted.item() + sum(self.output_sizes[:i])])  # add the maximum prediction and the corrected class
-            else:
-                max_preds.append([max_value.item(),self.model_targets[i][predicted.item()]])  # add the maximum prediction and the corrected class
-        
-        # find the most confident prediction
-        max = None
-        for max_pred in max_preds:
-            if max == None:
-                max = max_pred
-            else:
-                if max_pred[0] > max[0]:
-                    max = max_pred
-
-        return max[1] # return the predicted class
-
-    def validate(self, dir_path):
-        correct = 0
-        total = 0
-        alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-        # loop through every letter/class in alphabet
-        for class_num, letter in enumerate(alphabet):
-            # set the class directory path
-            class_dir_pth = dir_path + "/" + letter
-
-            # get all the jpg files in the class directory
-            files = os.listdir(class_dir_pth)
-            jpg_files = [file for file in files if file.lower().endswith('.jpg')]
-
-            for file in jpg_files:
-                image = JPGtoTensor(class_dir_pth + "/" + file,device=self.device)
-                predicted = self.classify(image)
-                total += 1
-                if predicted == class_num:
-                    correct += 1
-                else:
-                    alphabet = string.ascii_uppercase
-                    print("Predicted: " + alphabet[predicted] + "\tActual: " + alphabet[class_num])
-
-        accuracy = 100 * correct / total
-
-        return accuracy
-
-    # save all models to standard file location
-    def save(self):
-        for i in range(self.num_models):
-            save_model(self.models[i],"models/model_" + str(i) + ".pth")
-    
-    # load previously saved models
-    def load(self,num_models, device,input_size=28*28, num_layers=3, layer_sizes=[28*28,28*28,28*28], activation=F.relu, dropout_rate = 0):
-        self.num_models = num_models
-        self.models = []
-        for i in range(self.num_models):
-            self.models.append(LetterClassifier(input_size=input_size, num_layers=num_layers, layer_sizes=layer_sizes, output_size=self.output_sizes[i], activation=activation, dropout_rate = dropout_rate))
-            self.models[i].load_state_dict(torch.load("models/model_" + str(i) + ".pth"))
-            self.models[i].to(device)
-
-
-#### MAIN ####
-if __name__ == "__main__":
-    try:
-        # set constant training values
-        no_epochs = 30
-        min_loss_chng = 0.02
-        learn_rate = 0.01
-        batch_size = 32
-        num_models = 1
-
-        # target model list
-        alphabet = string.ascii_uppercase
-        # letters_list = [['D', 'O', 'G', 'Q', 'U', 'V', 'W', 'Y', 'Z', 'M', 'N', 'F', 'T'],
-        #         ['E', 'I', 'H', 'L', 'K', 'A', 'R', 'X', 'C', 'J', 'P', 'B', 'S']]
-        # letters_list = [['B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'P', 'R', 'S'], ['A', 'I', 'M', 'N', 'O', 'Q', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']]
-        # letters_list = [['M', 'N', 'W', 'H', 'V', 'U', 'R', 'P', 'Y'],
-        #                 ['O', 'C', 'D', 'Q', 'G', 'B', 'A', 'K'],
-        #                 ['E', 'F', 'X', 'T', 'I', 'L', 'J', 'S', 'Z']]
-        letters_list = []
-        positions_list = [[alphabet.index(letter) for letter in sublist] for sublist in letters_list]
-
-        # get the device
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-        # initialise model
-        comb_classifier = CombLetterClassifier(num_models,device,create_models=False,model_targets=positions_list)
-
-        # load last model if exists and user requests it
-        loaded = False
-        load_new = input("Would you like to load the previous model (y/n):")
-        if load_new == "y":
-            # File path exists, proceed with loading the model
-            comb_classifier.load(num_models,device)
-            print("Model loaded successfully.")
-            loaded = True
-            
-        # train the model if not loaded
-        if loaded == False:
-
-            comb_classifier = CombLetterClassifier(num_models,device,model_targets=positions_list)
-
-            print("Loading Data.")
-            comb_classifier.createDataLoaders("data/kaggle/kaggle_letters.csv",batch_size)
-            print("Data Loaded.")
-
-            print("Training Models.")
-            comb_classifier.train(no_epochs,min_loss_chng,learn_rate,display=True)
-
-            # ask if teh user would like to save the current model
-            save = input("Would you like to overwrite the last model (y/n):")
-            if save == "y":
-                comb_classifier.save()
-                print("Model saved successfully.")
-
-        # test final model on handcrafted test data
-        accuracy = comb_classifier.validate("data/hand_crafted")
-        print("Combined Model Test Accuracy: {:.4f}".format(accuracy))
-
-        # get file path of digit to predict
-        file_path = input("Please enter a filepath (\"q\" to quit): ")
-        while file_path != "q":
-            if file_path != "":
-                image = JPGtoTensor(file_path,device)
-                pred_class = comb_classifier.classify(image)
-                print("Im pretty sure its",alphabet[pred_class])
-            file_path = input("Please enter a filepath (\"q\" to quit): ")
-
-    except KeyboardInterrupt:
-        print("\nKeyboard interrupt detected. Exiting...")
-        torch.cuda.empty_cache()
-        print("GPU memory released.")
-        sys.exit(1)
---
+++ scripts/testing.py
@@ -0,0 +1,188 @@
+import building
+import matplotlib.gridspec as gridspec
+import torch.nn.functional as F
+import csv
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import numpy as np
+import torch
+
+def save_trends(val_trend, train_trend, loss_trend, epochs, test_acc, no_layers, activation_type, learning_rate,dir_save_path = "results/trends/test_"):
+    # ensure both arrays have the same length
+    if len(val_trend) != len(train_trend):
+        raise ValueError("Arrays must have the same length")
+
+    # zip the arrays together
+    data = zip(val_trend, train_trend, loss_trend, epochs)
+
+    # create file_name and path
+    file_path = dir_save_path
+    file_path += str(no_layers) + "_"
+    file_path += str(activation_type.__name__.replace("_", "")) + "_"
+    file_path += str(learning_rate)[2:] + "_"
+    file_path += str(test_acc).replace('.', '_') + ".csv"
+
+    # write the data to a CSV file
+    with open(file_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['validation_tren', 'training_trend', 'loss_trend', 'epochs']) 
+        writer.writerows(data)
+
+def read_trends_and_plot(csv_file,dir_path="results/plots/"):
+    # extract test parameters from the file name
+    file_name = csv_file.split("/")[-1].replace('.csv', '')  # extract only the file name and remove extension
+    params = file_name.split("_")
+    no_layers = int(params[1])
+    activation_type = params[2]
+    learning_rate = float("0." + params[3])
+    test_acc = float(params[4] + "." + params[5])
+
+    # read data from CSV
+    val_trend = []
+    train_trend = []
+    loss_trend = []
+    epochs = []
+    with open(csv_file, mode='r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            val_trend.append(float(row[0]))
+            train_trend.append(float(row[1]))
+            loss_trend.append(float(row[2]))
+            epochs.append(float(row[3]))
+
+    # plot trends
+    plt.figure(figsize=(10, 10))
+
+    # define gridspec
+    gs = gridspec.GridSpec(2, 1, height_ratios=[2, 1])
+
+    # top subplot for validation and training trends
+    ax1 = plt.subplot(gs[0])
+    ax1.plot(epochs, val_trend, label='Validation Trend')
+    ax1.plot(epochs, train_trend, label='Training Trend')
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Accuracy')
+    ax1.set_title(f'Validation and Training Trends\nLayers: {no_layers}, Activation: {activation_type}, Learning Rate: {learning_rate}, Test Accuracy: {test_acc}')
+    ax1.legend()
+
+    # bottom subplot for loss trend
+    ax2 = plt.subplot(gs[1])
+    ax2.plot(epochs, loss_trend, label='Loss Trend', color='green')
+    ax2.set_xlabel('Epochs')
+    ax2.set_ylabel('Loss')
+    ax2.set_title('Loss Trend')
+    ax2.legend()
+
+    # adjust layout
+    plt.tight_layout()
+
+    # save plot as a figure
+    plot_file_name = dir_path + os.path.basename(csv_file)[:-4] + ".png"
+    plt.savefig(plot_file_name)
+    plt.close()
+   
+def traverse_and_plot(directory,dir_save_path="results/plots/"):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".csv"):
+                csv_file = os.path.join(root, file)
+                read_trends_and_plot(csv_file,dir_path=dir_save_path)
+
+if __name__ == "__main__":
+    no_epochs = 1
+    min_loss_chng = 0.02
+    learning_rates = [0.001,0.01,0.1]
+
+    # get the device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # check if user wants to keep old tests
+    keep = input("Would you like to delete previous test? (y/n):")
+    if keep == "y":
+        sure = input("Are you sure ? (y/n):")
+        if sure == "y":
+            for root, dirs, files in os.walk("results"):
+                # Iterate over each file and delete it
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    os.remove(file_path)
+    print("Deleted old results.")
+
+    # set tha test parameters
+    no_layers = [1,2,3]
+    activation_types = [F.relu,F.leaky_relu,F.elu]
+
+    # load the data
+    x_train, y_train, x_valid, y_valid,  x_test, y_test = building.getProcessedData()
+    train_dataset,val_dataset,test_dataset = building.makeDatasets(x_train,y_train,x_valid, y_valid, x_test,y_test)
+    # load onto device
+    train_dataset = [(x.to(device), y.to(device)) for x, y in train_dataset]
+    val_dataset = [(x.to(device), y.to(device)) for x, y in val_dataset]
+    test_dataset = [(x.to(device), y.to(device)) for x, y in test_dataset]
+    train_loader, val_loader, test_loader = building.make_DataLoaders(train_dataset,val_dataset,test_dataset,64)
+
+    print("Loaded Data.")
+
+    # initialise pandas dataframe
+    df = pd.DataFrame(columns=['activation_function', 'no_layers','learning_rate', 'test_accuracy'])
+
+    print("Starting training process.")
+
+    for n_l in no_layers:
+        for acc_type in activation_types:
+            for l_r in learning_rates:
+                # create and train model
+                model = building.NumberClassifier(num_layers=n_l,activation=acc_type)
+                model.to(device)
+                validation_trend, training_trend, loss_trend, epochs = building.train_classifier(model,train_loader,val_loader,no_epochs,min_loss_chng,l_r)
+
+                # print succesfule training
+                print("Trained with " + acc_type.__name__ + " with " + str(n_l) + " layers at learning rate of " + str(l_r))
+
+                # validate model
+                acc = building.validate_classifier(model,test_loader)
+
+                # save trends to a csv
+                save_trends(validation_trend,training_trend,loss_trend,epochs,acc,n_l,acc_type,l_r)
+
+                # add data to the pandas df
+                record = {
+                        'activation_function': acc_type.__name__,
+                        'no_layers': n_l,
+                        'learning_rate': l_r,
+                        'test_accuracy': acc
+                    }
+                df = pd.concat([df, pd.DataFrame([record])], ignore_index=True)
+
+
+    traverse_and_plot("results/trends")
+    df.to_csv("results/test_accuracies.csv", index=False)
+
+
+
+    # test best with dropout
+    # models = [[F.leaky_relu,3,0.01],[F.leaky_relu,2,0.01],[F.elu,2,0.01]]
+    # df = pd.DataFrame(columns=['activation_function', 'no_layers', 'layer_sizes', 'learning_rate', 'test_accuracy'])
+    # for params in models:
+    #     model = classifier.NumberClassifier(num_layers=params[1],layer_sizes=layer_sizes[:params[1]],activation=params[0],dropout_rate=0.2)
+    #     validation_trend, training_trend, loss_trend, epochs = classifier.train_classifier(model,train_loader,val_loader,no_epochs,min_loss_chng,params[2])
+
+    #     # print succesfule training
+    #     print("Trained with " + params[0].__name__ + " with " + str(params[1]) + " layers at learning rate of " + str(params[2]))
+
+    #     # validate model
+    #     acc = classifier.validate_classifier(model,test_loader)
+
+    #     # save trends to a csv
+    #     save_trends(validation_trend,training_trend,loss_trend,epochs,acc,params[1],params[0],params[2],dir_save_path="results/drop_out_trends/dropout_")
+
+    #     record = {
+    #                 'activation_function': params[0].__name__,
+    #                 'no_layers': params[1],
+    #                 'layer_sizes': layer_sizes[:params[1]],
+    #                 'learning_rate': params[2],
+    #                 'test_accuracy': acc
+    #             }
+    #     df = pd.concat([df, pd.DataFrame([record])], ignore_index=True)
